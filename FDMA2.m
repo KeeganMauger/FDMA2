277ddd39aea438ecb40964761118b2a89fc317ef
KeeganMauger <keeganmauger@cmail.carleton.ca>
2022-02-18 12:21:53 -0500

    Starting to implement analytical solution

--- FDMA2.m
+++ FDMA2.m
@@ -8,6 +8,7 @@ clc;
 set(0,'DefaultFigureWindowStyle','docked');
 
 % Solving V=V0 @ x=0 and V=0 @ x=L in region LxW
+% Implement funtion 'pbaspect' to fix Z aspect ratio
 
 L = 90;
 W = 2/3 * L;
@@ -23,26 +24,34 @@ for i = 1:nx                %Iteration through length
     for j = 1:ny            %Iteration through width
         n = j + (i-1)*ny;
 
-        if i == 1;
+        if i == 1          % x=0 BCs
             G(n,:) = 0;
             G(n,n) = 1;
             F(n) = 1;
-        elseif i == nx
+        elseif i == nx     % x=1 BCs
             G(n,:) = 0;
             G(n,n) = 1;
-            F(n) = 0;
-%         elseif j == 1
-%             nxm = j + (i-2)*ny;
-%             nxp = j + (i)*ny;
-%             nyp = j+1 + (i-1)*ny;
-%             
-%             G(n,n) = 1;
-%         elseif j == ny
-%             nxm = j + (i-2)*ny;
-%             nxp = j + (i)*ny;
-%             nym = j-1 + (i-1)*ny;
-%             
-%             G(n,n) = 1;
+            %F(n) = 0;       %F(n)=0 sets z at final width to 0
+            
+% COMMENT BELOW FOR 1a
+            F(n) = 1;       %F(n)=1 sets z at final width to 1
+            
+        elseif j == 1                 % y=0 BCs
+            nxm = j + (i-2)*ny;
+            nxp = j + (i)*ny;
+            nyp = j+1 + (i-1)*ny;
+            
+            G(n,n) = 1;
+        elseif j == ny                % y=1 BCs
+            nxm = j + (i-2)*ny;
+            nxp = j + (i)*ny;
+            nym = j-1 + (i-1)*ny;
+            
+            G(n,n) = 1;
+            
+            
+% COMMENT ABOVE FOR 1a
+            
         else
             nxm = j + (i-2)*ny;
             nxp = j + (i)*ny;
@@ -73,7 +82,7 @@ end
 
 figure(2)
 surf(Vmap)
-
+pbaspect([1 1 0.5])
 
 
 
